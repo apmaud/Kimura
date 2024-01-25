571da986ab0aa393dae3b4d2d6e023ed7fc695f1
Arjun <apm526@nyu.edu>
2024-01-25 16:04:04 -0600

    Add welcome message

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,19 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import AuthPage from './pages/AuthPage'
 import Navbar from './components/overlay/Navbar'
+import LandingPage from './pages/LandingPage'
 
 function App() {
 
   return (
-    <div className='h-full min-h-full'>
+    <div className='h-full min-h-screen min-w-screen flex-col'>
       <Navbar />
       <Routes>
         <Route 
           path="/" 
           element={
-            <>
-              <AuthPage />
-            </>
-          } 
+            <LandingPage />
+          }
         />
       </Routes>
     </div>
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div className="hero min-h-screen bg-base-200 rounded-md shadow-lg">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Welcome to Kimura</h1>
+          <p className="py-6">A jiu-jitsu web application, centralizing techniques in one place. Sign up for access to resources and more!</p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
